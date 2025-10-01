a1e432c54bd4871f39311198af9270fb9beaedf8
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-10-01 22:22:33 +0700

    Percobaan 1

---
+++ jobsheet5/IfCetakKRSNoPresensi.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class IfCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS Siakad ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+        
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silakan Cetak KRS dan Minta Tanda tangan DPA");
+        }
+
+        sc.close();
+    }
+}
