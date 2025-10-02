fa827eb2fa108d3feeb1c294e18f3065f67afa7d
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-10-02 13:46:14 +0700

    Tugas 3

---
+++ jobsheet5/WifiKampus.java
@@ -0,0 +1,31 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class WifiKampus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Jenis pengguna (dosen/mahasiswa/lain): ");
+        String jenisPengguna = sc.nextLine();
+
+        if (jenisPengguna.equalsIgnoreCase("dosen")) {
+            System.out.println("Akses WiFi diberikan (dosen)");
+        } else {
+            if (jenisPengguna.equalsIgnoreCase("mahasiswa")) {
+                System.out.print("Masukkan jumlah SKS: ");
+                int sks = sc.nextInt();
+
+                if (sks >= 12) {
+                    System.out.println("Akses WiFi diberikan (mahasiswa aktif)");
+                } else {
+                    System.out.println("Akses ditolak, SKS kurang dari 12");
+                }
+            } else {
+                System.out.println("Akses ditolak");
+            }
+        }
+
+        sc.close();
+    }
+}
