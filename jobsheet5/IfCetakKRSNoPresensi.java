c7dc23deca32141d8146774e295be7e7e8b39ceb
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-10-02 07:42:20 +0700

    Percobaan 1

--- jobsheet5/IfCetakKRSNoPresensi.java
+++ jobsheet5/IfCetakKRSNoPresensi.java
@@ -8,11 +8,14 @@ public class IfCetakKRSNoPresensi {
 
         System.out.println("--- Cetak KRS Siakad ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
-        boolean uktLunas = sc.nextBoolean();
         
+        boolean uktLunas = sc.nextBoolean();
+
         if (uktLunas) {
             System.out.println("Pembayaran UKT Terverifikasi");
             System.out.println("Silakan Cetak KRS dan Minta Tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
 
         sc.close();
